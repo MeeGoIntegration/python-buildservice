c47bcd5f4c7e0e29d32d3f2106ece344f208eed1
Islam Amer <islam.amer@nokia.com>
2011-04-22 18:20:36 +0300

    another missed patch from master repo

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 from distutils.core import setup
 import os, sys
 
+static_files=[]
+static_files.append((os.path.join('/etc','boss'), ['conf/oscrc']))
+
 setup(name = 'buildservice',
-  version = '0.3',
-  description = 'access build service data',
-  author = 'Anas Nashif',
-  author_email = 'anas.nashif@intel.com',
-  url = 'http://meego.gitorious.org/meego-infrastructure-tools/python-buildservice',
-  packages = ['buildservice'],
+      version = '0.3',
+      description = 'Module to access OBS server',
+      author = 'Anas Nashif',
+      author_email = 'anas.nashif@intel.com',
+      url = 'http://meego.gitorious.org/meego-infrastructure-tools/python-buildservice',
+      packages = ['buildservice'],
+      data_files = static_files,
 )

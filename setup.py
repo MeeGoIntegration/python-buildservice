0b14ee55aab78a053b5a898d71d32006c107bafc
Li Yi <yix.li@intel.com>
2011-05-12 15:07:01 +0800

    bump version to 0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ static_files=[]
 static_files.append((os.path.join('/etc','boss'), ['conf/oscrc']))
 
 setup(name = 'buildservice',
-      version = '0.3',
+      version = '0.4',
       description = 'Module to access OBS server',
       author = 'Anas Nashif',
       author_email = 'anas.nashif@intel.com',

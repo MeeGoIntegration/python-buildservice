05138db099f347c2afe666b7937e4fbb7d521c97
Li Yi <yix.li@intel.com>
2011-06-03 18:37:08 +0800

    bump version to 0.5

--- setup.py
+++ setup.py
@@ -9,8 +9,16 @@ static_files.append((os.path.join('/etc','boss'), ['conf/oscrc']))
 if sys.version_info[:2] <= (2, 5) and '--install-layout=deb' in sys.argv:
     del sys.argv[sys.argv.index('--install-layout=deb')]
 
+version_path = 'VERSION'
+try:
+    # first line should be the version number
+    version = open(version_path).readline().strip()
+except IOError:
+    print 'WARNING: Cannot write version number file'
+    pass
+
 setup(name = 'buildservice',
-      version = '0.4',
+      version = version,
       description = 'Module to access OBS server',
       author = 'Anas Nashif',
       author_email = 'anas.nashif@intel.com',
